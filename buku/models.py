84380c6090a915918b66c0b026a8935c605fa324
EmiliaSumbawa <emiliasumbawa7@gmail.com>
2022-11-13 21:55:36 +0800

    Django

---
+++ buku/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Kategori(models.Model):
+    kategori = models.CharField(max_length=100)
+    def __str__(self):
+        return self.kategori
+        
+class Post(models.Model):
+    judul=models.CharField(max_length=250)
+    sinopsis=models.TextField()
+    kategori=models.ForeignKey(Kategori, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return self.judul
